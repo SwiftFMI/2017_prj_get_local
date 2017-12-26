0f00ab2560f43e5df59dae6d05ceca5642ed159b
Petar Ivanov <pepincho95@gmail.com>
2017-12-26 16:53:28 +0200

    Add initial project state

---
+++ getLocal/getLocal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  getLocal
+//
+//  Created by Petar Ivanov on 12/26/17.
+//  Copyright © 2017 Petar Ivanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
