bca5c74239c4d9882176bcf211449052bb03b9dc
Petar Ivanov <pepincho95@gmail.com>
2018-02-19 20:37:48 +0200

    Add ChooseCategory View Controller

---
+++ getLocal/getLocal/Controller/ChooseCategoryViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ChooseCategoryViewController.swift
+//  getLocal
+//
+//  Created by Petar Ivanov on 2/19/18.
+//  Copyright © 2018 Petar Ivanov. All rights reserved.
+//
+
+import UIKit
+
+class ChooseCategoryViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
