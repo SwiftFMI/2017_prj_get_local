5e28dfe087c92dda4ed92a4bf66262ff98a33322
Petar Ivanov <pepincho95@gmail.com>
2018-02-19 20:41:32 +0200

    Add SetObjectName View Controller

---
+++ getLocal/getLocal/Controller/SetObjectNameViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SetObjectNameViewController.swift
+//  getLocal
+//
+//  Created by Petar Ivanov on 2/19/18.
+//  Copyright © 2018 Petar Ivanov. All rights reserved.
+//
+
+import UIKit
+
+class SetObjectNameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
