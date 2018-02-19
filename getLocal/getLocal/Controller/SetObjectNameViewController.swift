7f697e567a109045198c4ef2aaa05091864478bf
Petar Ivanov <pepincho95@gmail.com>
2018-02-19 22:28:47 +0200

    Add text input field in SetNameObject view controller

--- getLocal/getLocal/Controller/SetObjectNameViewController.swift
+++ getLocal/getLocal/Controller/SetObjectNameViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class SetObjectNameViewController: UIViewController {
     
+    @IBOutlet weak var objectNameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
