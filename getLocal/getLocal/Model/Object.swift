800a6f0570b99c5fb5181c9a7731de4cfbb91eb0
Latchezar Mladenov <latchezar.mladenov@dision.co>
2018-02-20 15:24:33 +0200

    Update model

--- getLocal/getLocal/Model/Object.swift
+++ getLocal/getLocal/Model/Object.swift
@@ -11,18 +11,27 @@ import FirebaseDatabase
 
 class Object {
 	
-		var ref: DatabaseReference?
-		var title: String?
-		var latitude: String?
-		var longitude: String?
+	var ref: DatabaseReference?
+	var title: String?
+	var latitude: Double?
+	var longitude: Double?
+	var category: String?
+	var imageUrl: String?
+	var description: String?
+	var workTime: String?
+	var uid: String?
 	
-		init (snapshot: DataSnapshot) {
-			ref = snapshot.ref
-			
-			let data = snapshot.value as! Dictionary<String, String>
-			title = data["title"]
-			latitude = data["latitude"]
-			longitude = data["longitude"]
-		}
+	init (snapshot: DataSnapshot) {
+		ref = snapshot.ref
+		let data = snapshot.value as! Dictionary<String, Any>
+		title = data["title"] as? String
+		latitude = data["latitude"] as? Double
+		longitude = data["longitude"] as? Double
+		category = data["category"] as? String
+		imageUrl = data["imageUrl"] as? String
+		description = data["description"] as? String
+		workTime = data["workTime"] as? String
+		uid = data["uid"] as? String
+	}
 	
 }
