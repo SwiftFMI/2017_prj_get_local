978e7a2b5184fffe1042eeb661137dd40f830610
Petar Ivanov <pepincho95@gmail.com>
2018-02-21 18:33:34 +0200

    Add picker view functionality

--- getLocal/getLocal/Controller/ChooseObjectCategoryViewController.swift
+++ getLocal/getLocal/Controller/ChooseObjectCategoryViewController.swift
@@ -8,14 +8,24 @@
 
 import UIKit
 
-class ChooseObjectCategoryViewController: UIViewController {
+class ChooseObjectCategoryViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    
+    @IBOutlet weak var categoryPickerView: UIPickerView!
     
     var objectImageUrl: String = ""
+    
+    var pickerData: [String] = [String]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        pickerData = ["restaurant", "club", "pharmacy", "shop", "services", "other"]
+        
+        // Connect data
+        self.categoryPickerView.delegate = self
+        self.categoryPickerView.dataSource = self
     }
     
     override func didReceiveMemoryWarning() {
@@ -24,11 +34,27 @@ class ChooseObjectCategoryViewController: UIViewController {
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let objectCategory: String = "bars"
+//        let objectCategory: String = "bars"
+        let objectCategory: String = pickerData[categoryPickerView.selectedRow(inComponent: 0)]
         
         if let setObjectNameVC = segue.destination as? SetObjectNameViewController {
             setObjectNameVC.objectImageUrl = objectImageUrl
             setObjectNameVC.objectCategory = objectCategory
         }
+    }
+    
+    // The number of columns of data
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    // The data to return for the row and component (column) that's being passed in
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
     }
 }
