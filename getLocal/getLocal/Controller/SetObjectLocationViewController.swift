2d33c671bc912b22e7f3f2bb602653e2acc37bf4
Petar Ivanov <pepincho95@gmail.com>
2018-02-21 17:21:26 +0200

    Add set pin on change location

--- getLocal/getLocal/Controller/SetObjectLocationViewController.swift
+++ getLocal/getLocal/Controller/SetObjectLocationViewController.swift
@@ -16,64 +16,39 @@ class SetObjectLocationViewController: UIViewController, MKMapViewDelegate, CLLo
     @IBOutlet weak var objectLocationMapView: MKMapView!
     
     var locationManager =  CLLocationManager()
-    let newPin = MKPointAnnotation()
+    var newPin = MKPointAnnotation()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        self.title = "Set object's location"
+        
+        objectLocationMapView.delegate = self
+        
+        objectLocationMapView.showsUserLocation = false
         
         // User's location
-        
-//        locationManager.delegate = self
-//        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-//        if #available(iOS 8.0, *) {
-//        locationManager.requestAlwaysAuthorization()
-//        } else {
-//            // Fallback on earlier versions
-//        }
-        
-//        if CLLocationManager.locationServicesEnabled() {
-            //locationManager.startUpdatingHeading()
-//            locationManager.startUpdatingLocation()
-//        }
+        determineCurrentLocation()
         
         // add gesture recognizer
-//        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(SetObjectLocationViewController.mapLongPress(_:))) // colon needs to pass through info
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(SetObjectLocationViewController.mapLongPress(_:))) // colon needs to pass through info
 //        longPress.minimumPressDuration = 1.5 // in seconds
-//        //add gesture recognition
-//        objectLocationMapView.addGestureRecognizer(longPress)
+        longPress.numberOfTouchesRequired = 1
+
+        //add gesture recognition
+        objectLocationMapView.addGestureRecognizer(longPress)
     }
     
-    // func called when gesture recognizer detects a long press
-//    @objc func mapLongPress(_ recognizer: UIGestureRecognizer) {
-//        print("A long press has been detected.")
-//
-//        let touchedAt = recognizer.location(in: self.objectLocationMapView) // adds the location on the view it was pressed
-//        let touchedAtCoordinate : CLLocationCoordinate2D = objectLocationMapView.convert(touchedAt, toCoordinateFrom: self.objectLocationMapView) // will get coordinates
-//
-//        let newPin = MKPointAnnotation()
-//        newPin.coordinate = touchedAtCoordinate
-//        objectLocationMapView.addAnnotation(newPin)
-//    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        determineCurrentLocation()
-    }
-    
-    func determineCurrentLocation()
-    {
+    func determineCurrentLocation() {
         locationManager = CLLocationManager()
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestAlwaysAuthorization()
         
-        if CLLocationManager.locationServicesEnabled() {
-            //locationManager.startUpdatingHeading()
-            locationManager.startUpdatingLocation()
-        }
+//        if CLLocationManager.locationServicesEnabled() {
+//            locationManager.startUpdatingHeading()
+        locationManager.startUpdatingLocation()
+//        }
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -81,21 +56,14 @@ class SetObjectLocationViewController: UIViewController, MKMapViewDelegate, CLLo
 //
 //        let location = locations.last! as CLLocation
 //
-//        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-//        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-//
-//        //set region on the map
-//        objectLocationMapView.setRegion(region, animated: true)
-//
 //        newPin.coordinate = location.coordinate
 //        objectLocationMapView.addAnnotation(newPin)
-        
         
         let userLocation:CLLocation = locations[0] as CLLocation
         
         // Call stopUpdatingLocation() to stop listening for location updates,
         // other wise this function will be called every time when user location changes.
-        //manager.stopUpdatingLocation()
+        locationManager.stopUpdatingLocation()
         
         let center = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
         let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
@@ -103,11 +71,25 @@ class SetObjectLocationViewController: UIViewController, MKMapViewDelegate, CLLo
         objectLocationMapView.setRegion(region, animated: true)
         
         // Drop a pin at user's Current Location
-        let myAnnotation: MKPointAnnotation = MKPointAnnotation()
-        myAnnotation.coordinate = CLLocationCoordinate2DMake(userLocation.coordinate.latitude, userLocation.coordinate.longitude);
-        myAnnotation.title = "Current location"
-        objectLocationMapView.addAnnotation(myAnnotation)
+//        let myAnnotation: MKPointAnnotation = MKPointAnnotation()
+        newPin.coordinate = CLLocationCoordinate2DMake(userLocation.coordinate.latitude, userLocation.coordinate.longitude);
+//        myAnnotation.title = "Current location"
+        objectLocationMapView.addAnnotation(newPin)
+    }
+    
+    // func called when gesture recognizer detects a long press
+    @objc func mapLongPress(_ recognizer: UIGestureRecognizer) {
+        print("A long press has been detected.")
         
+        objectLocationMapView.removeAnnotation(newPin)
+        newPin = MKPointAnnotation()
+        
+        let touchedAt = recognizer.location(in: self.objectLocationMapView) // adds the location on the view it was pressed
+        let touchedAtCoordinate : CLLocationCoordinate2D = objectLocationMapView.convert(touchedAt, toCoordinateFrom: self.objectLocationMapView) // will get coordinates
+        
+//        let newPin = MKPointAnnotation()
+        newPin.coordinate = touchedAtCoordinate
+        objectLocationMapView.addAnnotation(newPin)
     }
     
     override func didReceiveMemoryWarning() {
