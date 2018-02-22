a38349a5c740c09bd88f69cc991323cd321dfd3f
Emil Iliev <emil_iliev1994@abv.bg>
2018-02-22 21:59:25 +0200

    Added tabbar images, reworked profile, localization etc..

--- getLocal/getLocal/Controller/LoginViewController.swift
+++ getLocal/getLocal/Controller/LoginViewController.swift
@@ -14,11 +14,12 @@ import SwiftOverlays
 
 class LoginViewController: UIViewController {
 	
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         autologin()
-        addLogin()
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,21 +41,37 @@ class LoginViewController: UIViewController {
         }
     }
     
-    private func addLogin() {
-        //creating button
-        let loginButton = FBSDKLoginButton()
-        loginButton.delegate = self
-        loginButton.center = view.center
-        
-        //adding it to view
-        view.addSubview(loginButton)
+    @IBAction func handleLogin () {
+        let loginManager = FBSDKLoginManager()
+        loginManager.logIn(withReadPermissions: ["public_profile", "email"], from: self) { [weak self] (fbResponse, fbError) in
+            guard let `self` = self ,let result = fbResponse else{ return }
+            if result.isCancelled { //if user taps DONE
+                return
+            }
+            
+            if let current = FBSDKAccessToken.current() {
+                let token = current.tokenString
+                let credential = FacebookAuthProvider.credential(withAccessToken: token!)
+                Auth.auth().signIn(with: credential) { (user, error) in
+                    if let error = error {
+                        self.removeAllOverlays()
+                        print("Error:\(error.localizedDescription)")
+                        return
+                    }
+                    
+                    self.removeAllOverlays()
+                    print("User is signed in successfully")
+                    // User is signed in
+                    let mainVC = self.storyboard?.instantiateViewController(withIdentifier: StoryboardIDS.mainVC.rawValue)
+                    self.present(mainVC!, animated: true, completion: nil)
+                }
+            } else {
+                self.removeAllOverlays()
+            }
+        }
     }
-}
-
-// MARK: - FBSDKLoginButtonDelegate
-extension LoginViewController: FBSDKLoginButtonDelegate {
-	
-		func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
 			self.showWaitOverlay()
 			if let error = error {
 				self.removeAllOverlays()
